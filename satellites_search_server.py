eb6a5aa8371b613777b6c854516c0d2da0fc7ed3
Wojtek Kłusek <wojtek.klusek2@gmail.com>
2025-08-16 11:47:42 +0200

    logic changes

--- satellites_search_server.py
+++ satellites_search_server.py
@@ -9,7 +9,7 @@ mcp = FastMCP("satellites_searcher")
 DEFAULT_TLE = 'https://celestrak.org/NORAD/elements/stations.txt'
 
 
-# tip: do not try to pass pydantic class with parameters inside, it will cause errors
+# tip: do not try to pass pydantic class here, it will cause errors
 # provide parameters directly like below
 @mcp.tool()
 async def get_satellite_passes(
@@ -20,10 +20,10 @@ async def get_satellite_passes(
     tle_url: Annotated[str, Field(description="URL of the TLE file to download")] = DEFAULT_TLE,
     timezone_name: Annotated[str, Field(description="Timezone for the observer (default: UTC)")] = 'UTC',
     elevation_m: Annotated[int, Field(description="Observer's elevation in meters (default: 200)")] = 200,
-    min_above_horizon_deg: Annotated[int, Field(description="Minimum satellite altitude above horizon in degrees (default: 20)")] = 20,
+    min_culmination_altitude_deg: Annotated[int, Field(description="Minimum satellite altitude above horizon in degrees (default: 15)")] = 15,
 ) -> str:
     """
-        Get satellite passes. Use this tool to calculate satellites passes.
+        Get the satellite passes. Use this tool to calculate the satellite passes.
     """
     print(f'Searching satellite passes. Timezone: {timezone_name}')
     satellite_search = SatelliteSearch(
@@ -32,10 +32,9 @@ async def get_satellite_passes(
         lat=float(lat),
         lon=float(lon),
         range_days=range_days,
-        calc_resolution_min=1,
         timezone_param=timezone_name,
         elev=elevation_m,
-        min_above_horizon_deg=min_above_horizon_deg
+        min_culmination_altitude_deg=min_culmination_altitude_deg
     )
     search_results_text = await satellite_search.calculate_satellites_nearby()
     return search_results_text
